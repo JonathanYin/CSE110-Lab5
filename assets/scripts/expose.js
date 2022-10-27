6e646fe2f8102837e88e8c94e09c3efc4ea71aab
JonathanYin <joyin@ucsd.edu>
2022-10-27 16:41:04 -0700

    Added event listener for volume slider

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -20,16 +20,24 @@ function init() {
   //     audio.play();
   //   });
   // });
-  const selectElement = document.getElementById("horn-select");
   const parentDOM = document.getElementById("expose");
+
+  const selectElement = document.getElementById("horn-select");
+  // horn selector
+  const slider = document.getElementById("volume");
+  // volume slider
+
   const img = document.querySelector("img"); 
+  // horn image
+
+  const volControl = document.getElementById("volume-controls");
+  const vol = volControl.querySelector("img");
+  // volume image
+
   const audio = document.getElementsByClassName("hidden");
 
-  console.log(img);
-  console.log("hello");
-  console.log(selectElement);
-
   selectElement.addEventListener("change", (event) => {
+    // change horn image and audio file
     if (event.target.value == "air-horn") {
       img.src = "assets/images/air-horn.svg";
       audio.src = "assets/audio/air-horn.mp3";
@@ -47,8 +55,19 @@ function init() {
     
   });
 
-}
-
-function changeVolume() {
-  
+  slider.addEventListener("change", (event) => {
+    // change volume
+    if (event.target.value == 0){
+      vol.src = "assets/icons/volume-level-0.svg";
+    }
+    else if (event.target.value >= 1 && event.target.value < 33){
+      vol.src = "assets/icons/volume-level-1.svg";
+    }
+    else if (event.target.value >= 33 && event.target.value < 67){
+      vol.src = "assets/icons/volume-level-2.svg";
+    }
+    else if (event.target.value >= 67){
+      vol.src = "assets/icons/volume-level-3.svg";
+    }
+  });
 }
