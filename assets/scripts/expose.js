beb272692e5d6d89c6e3ebcb36e153b8b77e1070
JonathanYin <joyin@ucsd.edu>
2022-10-27 14:50:52 -0700

    Added eventListener for selecting image and audio

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -4,4 +4,51 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  // var vol = document.getElementById("volume");
+  // var audio = document.getElementsByClassName("hidden");
+  // var play_Btn = document.querySelector("button");
+  // // console.log(vol);
+  // // console.log(audio);
+  
+  // vol.addEventListener("change", function(e) {
+  //   audio.volume = e.currentTarget.value / 100;
+  // })
+  
+  // buttons.forEach(button => {
+  //   button.addEventListener("click", () => {
+  //     console.log("hello");
+  //     audio.play();
+  //   });
+  // });
+  const selectElement = document.getElementById("horn-select");
+  const parentDOM = document.getElementById("expose");
+  const img = document.querySelector("img"); 
+  const audio = document.getElementsByClassName("hidden");
+
+  console.log(img);
+  console.log("hello");
+  console.log(selectElement);
+
+  selectElement.addEventListener("change", (event) => {
+    if (event.target.value == "air-horn") {
+      img.src = "assets/images/air-horn.svg";
+      audio.src = "assets/audio/air-horn.mp3";
+    }
+
+    if (event.target.value == "car-horn") {
+      img.src = "assets/images/car-horn.svg";
+      audio.src = "assets/audio/car-horn.mp3";
+    }
+
+    if (event.target.value == "party-horn") {
+      img.src = "assets/images/party-horn.svg";
+      audio.src = "assets/audio/party-horn.mp3";
+    }
+    
+  });
+
+}
+
+function changeVolume() {
+  
 }
